042e6ee5ba08b31d2e7f83711aa96a1418c206ee
Akash Verma <63094947+akaskverma2000@users.noreply.github.com>
2024-05-02 08:30:22 +0530

    Add Error Handling on User Registration/Login (#6)
    **Changes**
    
    - Added a new register endpoint in the users-permissions controller to handle user registration.
    - Updated the routes configuration to include the register endpoint.
    - Added error handling for cases where the username is already taken during registration.

--- public/js/components/loginForm.js
+++ public/js/components/loginForm.js
@@ -16,13 +16,11 @@ loginForm.addEventListener('submit', async (e) => {
     try {
         const data = await login(username, password);
 
-        if (data.jwt) {
+        if (data?.jwt) {
             localStorage.setItem('token', data.jwt);
             localStorage.setItem('userId', data.user.id);
             showToast('Great job! You have successfully logged into the web chat app.Welcome aboard!', true);
             window.location.href = 'http://127.0.0.1:8080/pages/chat.html';
-        } else {
-            showToast('Oops! Login failed. Please check your username and password and try again.', false);
         }
     } catch (error) {
         showToast('Oops! It looks like we are having trouble connecting to the server. Please check your internet connection and try again', false);
