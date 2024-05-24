d92b475b4bbefd91b42949f19e5ce459a4672618
Akash Verma <akashverma.av2019@gmail.com>
2024-05-24 23:28:30 +0530

    Update Web Socket Connection

--- public/js/utils/websocket.js
+++ public/js/utils/websocket.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://web-chat-app-nu.vercel.app');
+const ws = new WebSocket('wss://web-chat-app-nu.vercel.app/');
 
 /**
  * Logs a message to the console when the WebSocket connection is established.
